62c8c666de543e5002f1e239f7517143e4dfefc3
Zechariah Wang <zechariahwang@gmail.com>
2026-09-16 18:19:57 -0400

    lab

---
+++ Core/Inc/common.h
@@ -0,0 +1,16 @@
+/*
+ * common.h
+ *
+ *  Created on: Jan 5, 2024
+ *      Author: nexususer
+ *
+ *      NOTE: If you feel that there are common
+ *      C functions corresponding to this
+ *      header, then any C functions you write must go into a corresponding c file that you create in the Core->Src folder
+ */
+
+#ifndef INC_COMMON_H_
+#define INC_COMMON_H_
+
+
+#endif /* INC_COMMON_H_ */
---
+++ Core/Inc/k_task.h
@@ -0,0 +1,15 @@
+/*
+ * k_task.h
+ *
+ *  Created on: Jan 5, 2024
+ *      Author: nexususer
+ *
+ *      NOTE: any C functions you write must go into a corresponding c file that you create in the Core->Src folder
+ */
+
+#ifndef INC_K_TASK_H_
+#define INC_K_TASK_H_
+
+
+
+#endif /* INC_K_TASK_H_ */
